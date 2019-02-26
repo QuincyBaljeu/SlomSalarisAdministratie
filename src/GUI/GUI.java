6c5cc6b457a9105c286aedeafee02c35fc968bcf
Quincy <q.baljeu@gmail.com>
2019-02-26 19:02:03 +0100

    Added textfields to GUI, added button to GUI

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,18 +1,77 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
 
 public class GUI extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Main menu");
+        ArrayList<TextField> textFields = new ArrayList<>();
+
+        VBox labelVbox = new VBox();
+        VBox textFieldVbox = new VBox();
+        HBox hBox = new HBox();
+
+        BorderPane borderPane = new BorderPane();
+
+        //Components ID
+        Label IDLabel = new Label("Worker ID: ");
+        TextField IDTextField = new TextField();
+        textFields.add(IDTextField);
+
+        //components first name
+        Label firstNameLabel = new Label("First name: ");
+        TextField firstNameTextField = new TextField();
+        textFields.add(firstNameTextField);
+
+        //components last name
+        Label lastNameLabel = new Label("Last name: ");
+        TextField lastNameTextField = new TextField();
+        textFields.add(lastNameTextField);
+
+        //components salary
+        Label salaryLabel = new Label("Salary: ");
+        TextField salaryTextField = new TextField();
+        textFields.add(salaryTextField);
+
+        //components general
+        Button confirmData = new Button("Confirm data");
+        confirmData.setOnAction(event -> {
+            for(TextField textField : textFields){
+                if(textField.getText().isEmpty()){
+                    System.out.println("Leeg vak");
+                    return;
+                }
+            }
+        });
+
+        //finalizing scene
+        labelVbox.getChildren().addAll(IDLabel, firstNameLabel, lastNameLabel, salaryLabel);
+        textFieldVbox.getChildren().addAll(IDTextField, firstNameTextField, lastNameTextField, salaryTextField, confirmData);
+
+        hBox.getChildren().addAll(labelVbox, textFieldVbox);
+
+        labelVbox.setSpacing(8);
+        hBox.setSpacing(50);
+
+        borderPane.setLeft(hBox);
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
         stage.show();
     }
 
-
-    public static void mainMenu(){
-
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
+import Data.Chef;
 import Data.CommissionWorker;
+import Data.HourWorker;
+import Data.PieceWorker;
 
 import static javafx.application.Application.launch;
 
 public class Main {
 
     public static void main(String [] args){
-        CommissionWorker commissionWorker = new CommissionWorker(123,"karel", "anker", 2000.0, "commisioner", 0);
-        System.out.println(commissionWorker.getMonthlySalary());
-        //launch(GUI.GUI.class);
+        CommissionWorker commissionWorker = new CommissionWorker(123,"Jeff", "Hanneman", 2000.0, "commisioner", 0);
+        Chef chef = new Chef(456, "Tom", "Araya", 3000, "chef", 500, false);
+        HourWorker hourWorker = new HourWorker(678,"Dave", "Lombardo",5,"hourwoeker", 20);
+        PieceWorker pieceWorker = new PieceWorker(901, "Kerry", "King", 10, "piecer", 10);
+
+        launch(GUI.GUI.class);
     }
 }

ff557b8030c6f1c1a3dd3cb0952e842b66bbf8ce
Quincy <q.baljeu@gmail.com>
2019-02-26 17:51:57 +0100

    Added getMonthlySalary to PieceWorker

--- src/Data/PieceWorker.java
+++ src/Data/PieceWorker.java
@@ -2,11 +2,19 @@ package Data;
 
 public class PieceWorker extends Employee {
 
-    public PieceWorker(int ID, String firstName, String lastName, double salary, String position) {
+    public int amountOfPieces;
+
+    public PieceWorker(int ID, String firstName, String lastName, double salary, String position, int amountOfPieces) {
         this.ID = ID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.salary = salary;
         this.position = position;
+        this.amountOfPieces = amountOfPieces;
+    }
+
+    public double getMonthlySalary(){
+        double monthlySalary = this.salary + (5 * this.amountOfPieces);
+        return monthlySalary;
     }
 }

849a7c0cac589bdd73d1072d5149879544730ed7
Quincy <q.baljeu@gmail.com>
2019-02-26 16:52:10 +0100

    Changed interface Person to abstract class Emplyee. Added initial GUI

---
+++ src/GUI/GUI.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Main menu");
+        stage.show();
+    }
+
+
+    public static void mainMenu(){
+
+    }
+}

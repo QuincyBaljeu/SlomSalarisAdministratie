1dd5e5f1b9ac6ca27db77f10f011944f36fb1fd5
Quincy <q.baljeu@gmail.com>
2019-02-26 16:56:55 +0100

    Added attributes to Employee

--- src/Data/Employee.java
+++ src/Data/Employee.java
@@ -1,4 +1,13 @@
 package Data;
 
+import java.time.LocalDate;
+
 public abstract class Employee {
+    public int ID;
+    public String name;
+    public String adress;
+    public String residence;
+    public LocalDate dateOfBirth;
+    public LocalDate dateOfEnlistment;
+    public LocalDate dateOfTermination;
 }

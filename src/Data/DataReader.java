1a17d5c268dcf5387e68b4f239102b0dd126c3e0
Quincy <q.baljeu@gmail.com>
2019-03-25 19:14:43 +0100

    added reader methods to datareader

--- src/Data/DataReader.java
+++ src/Data/DataReader.java
@@ -1,4 +1,37 @@
 package Data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DataReader {
+
+    public void readDataFromTextfile(File file, ArrayList<Employee> employees){
+
+        Scanner scanner = null;
+
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        while(scanner.hasNextLine()){
+            Scanner employeeScanner = new Scanner(scanner.nextLine());
+        }
+    }
+
+
+    public void saveDataToTextfile(){
+
+    }
+
+    public void readDataFromObjectfile(File file, ArrayList<Employee> employees){
+
+    }
+
+    public void saveDataToObjectfile(){
+        
+    }
 }

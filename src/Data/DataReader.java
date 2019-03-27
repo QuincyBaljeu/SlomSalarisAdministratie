9b2f46232b4d0002201ec88f9eb6c78f9497b536
Quincy <q.baljeu@gmail.com>
2019-03-27 15:38:40 +0100

    begin implementatie data reader from text file

--- src/Data/DataReader.java
+++ src/Data/DataReader.java
@@ -10,43 +10,70 @@ public class DataReader {
 
     public void readDataFromTextfile(File file, ObservableList<Employee> employees){
 
-        try(Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()){
-                String employeeString = scanner.nextLine();
-                Scanner employeeScanner = new Scanner(employeeString);
-                employeeScanner.useDelimiter("#");
+        Scanner scanner = null;
 
-                while (employeeScanner.hasNext()){
-                    String position = employeeScanner.next();
-                    switch(position){
-                        case "Chef":
-                            int ID = employeeScanner.nextInt();
-                            String firstName = employeeScanner.next();
-                            String lastName = employeeScanner.next();
-                            double salary = employeeScanner.nextDouble();
-                            double bonus = employeeScanner.nextDouble();
-                            boolean bonusAssigned = employeeScanner.hasNextBoolean();
-
-                            employees.add(new Chef(ID, firstName, lastName, salary, position, bonus, bonusAssigned));
-                            break;
-
-                        case "CommissionWorker":
-
-                            break;
-
-                        case "HourWorker":
-
-                            break;
-
-                        case "PieceWorker":
-
-                            break;
-                    }
-                }
-            }
-
+        try{
+            scanner = new Scanner(file);
         } catch (FileNotFoundException e){
             e.printStackTrace();
+        }
+
+        while (scanner.hasNextLine()){
+            String employeeString = scanner.nextLine();
+            Scanner employeeScanner = new Scanner(employeeString);
+            employeeScanner.useDelimiter("#");
+
+            while (employeeScanner.hasNext()){
+                String position = employeeScanner.next();
+                int ID = employeeScanner.nextInt();
+                String firstName = employeeScanner.next();
+                String lastName = employeeScanner.next();
+                double salary = employeeScanner.nextDouble();
+
+                switch(position){
+                    case "Chef":
+//                        int ID = employeeScanner.nextInt();
+//                        String firstName = employeeScanner.next();
+//                        String lastName = employeeScanner.next();
+//                        double salary = employeeScanner.nextDouble();
+                        double bonus = employeeScanner.nextDouble();
+                        boolean bonusAssigned = employeeScanner.hasNextBoolean();
+
+                        employees.add(new Chef(ID, firstName, lastName, salary, position, bonus, bonusAssigned));
+                        break;
+
+                    case "Commission worker":
+//                        ID = employeeScanner.nextInt();
+//                        firstName = employeeScanner.next();
+//                        lastName = employeeScanner.next();
+//                        salary = employeeScanner.nextDouble();
+//                        int amountOfCommissions = scanner.nextInt();
+                        System.out.println(scanner.next());
+//
+//                        employees.add(new CommissionWorker(ID, firstName, lastName, salary, position, amountOfCommissions));
+                        break;
+
+                    case "Hour worker":
+//                        ID = employeeScanner.nextInt();
+//                        firstName = employeeScanner.next();
+//                        lastName = employeeScanner.next();
+//                        salary = employeeScanner.nextDouble();
+                        int hoursWorked = employeeScanner.nextInt();
+
+                        employees.add(new HourWorker(ID, firstName, lastName, salary, position, hoursWorked));
+                        break;
+
+                    case "Piece worker":
+//                        ID = employeeScanner.nextInt();
+//                        firstName = employeeScanner.next();
+//                        lastName = employeeScanner.next();
+//                        salary = employeeScanner.nextDouble();
+                        int amountOfPieces = employeeScanner.nextInt();
+
+                        employees.add(new PieceWorker(ID, firstName, lastName, salary, position, amountOfPieces));
+                        break;
+                }
+            }
         }
     }
 
